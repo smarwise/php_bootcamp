6d272e2f9ed731c836524776c7713a6d31900c19
Simbiso MARWISE <smarwise@e4r7p5.wethinkcode.co.za>
2018-10-04 09:53:22 +0200

    this✍️

---
+++ d02/ex01/one_more_time.php
@@ -0,0 +1,71 @@
+#!/usr/bin/php
+<?PHP
+
+function frtoeng($datenow)
+{
+    $datenow = str_replace("Lundi", "Monday", $datenow);
+    $datenow = str_replace("Mardi", "Tuesday", "$datenow");
+    $datenow = str_replace("Mercredi", "Wednesday", "$datenow");
+    $datenow = str_replace("Jeudi", "Thursday", "$datenow");
+    $datenow = str_replace("Vendredi", "Fridayy", "$datenow");
+    $datenow = str_replace("Samedi", "Saturday", "$datenow");
+    $datenow = str_replace("Dimanche", "Sunday", "$datenow");
+    $datenow = str_replace("Janvier", "January", "$datenow");
+    $datenow = str_replace("Février", "February", "$datenow");
+    $datenow = str_replace("Mars", "March", "$datenow");
+    $datenow = str_replace("Avril", "April", "$datenow");
+    $datenow = str_replace("Mail", "May", "$datenow");
+    $datenow = str_replace("Juin", "June", "$datenow");
+    $datenow = str_replace("Juillet", "July", "$datenow");
+    $datenow = str_replace("Août", "August", "$datenow");
+    $datenow = str_replace("Septembre", "September", "$datenow");
+    $datenow = str_replace("Octobre", "October", "$datenow");
+    $datenow = str_replace("Novembre", "November", "$datenow");
+    $datenow = str_replace("Décembre", "December", "$datenow");
+    return ($datenow);
+}
+
+function checkformat($datenow)
+{
+    $arrayweek = array("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday");
+    $arraymonth = array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
+    foreach ($arrayweek as $day)
+    {
+        if (strpos($datenow, $day) !== FALSE)
+        {
+            $i = 1;
+        }
+    }
+    foreach ($arraymonth as $day)
+    {
+        if (strpos($datenow, $day) !== FALSE)
+        {
+            $i = 1;
+        }
+    }
+    return ($i);
+}
+
+if ($argc == 1)
+    exit;
+$datenow = $argv[1];
+$datenow = frtoeng($datenow);
+$i = checkformat($datenow);
+if (!$i)
+{
+    echo "Wrong Format\n";
+    exit;
+}
+$time = strtotime("$datenow");
+preg_match("/[0-9][0-9]:[0-9][0-9]:[0-9][0-9]/", "$datenow", $array);
+//$sec = $array[0];
+//$result = strtotime($sec);
+//echo "$result";
+$n = 0;
+while (is_numeric($datenow[$n]) === FALSE)
+    $n++;
+$string = substr($datenow, $n);
+echo "$string\n";
+$time = strtotime($string);
+echo "$time";
+?>
