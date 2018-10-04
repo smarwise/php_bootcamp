326fc85be2a579112ac2ac59b32875d21050c5bb
Simbiso MARWISE <smarwise@e4r7p5.wethinkcode.co.za>
2018-10-04 13:48:53 +0200

    crazy

--- d02/ex02/magnifying_glass.php
+++ d02/ex02/magnifying_glass.php
@@ -1,10 +1,36 @@
 #!/usr/bin/php
 <?PHP
-$fd = fopen($argv[1], "r");
-while ($line = fgets($fd))
+
+$string = file($argv[1]);
+
+$results = preg_grep("/href=/", $string);
+print_r($results);
+$i = 1;
+while ($results[$i])
 {
-    $rn = $line;
-    preg_match("/a href=""/", "$datenow", $array);
-    echo "$line";
+    $str = $results[$i];
+    if (($pos = (strpos($str, "title="))) != NULL)
+    {
+        $pos = $pos + 7;
+       
+        while ($str[$pos] != '"')
+        {
+            $str[$pos] = strtoupper($str[$pos]);
+            $pos++;
+        }
+
+    }
+    $pos = strpos($str, "http");
+    while ($str[$pos] != '>')
+        $pos++;
+        while ($str[$pos] != '<')
+        {
+            $str[$pos] = strtoupper($str[$pos]);
+            $pos++;
+        }
+    $results[$i] = $str;
+    $i++;
 }
+print_r($results);
+ 
 ?>
