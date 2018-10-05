75425e1fc8e1e60be3b89416c9977ba1dbc24773
Simbiso MARWISE <smarwise@e5r6p11.wethinkcode.co.za>
2018-10-05 14:17:24 +0200

    awesome

--- d03/ex03/cookie_crisp.php
+++ d03/ex03/cookie_crisp.php
@@ -3,19 +3,19 @@
 //set cookie
 $action = $_GET['action'];
 
-if ($action == "set")
+if ($action === "set")
 {
 	$cookie_name = $_GET['name'];
 	$cookie_value = $_GET['value'];
 	setcookie($cookie_name, $cookie_value, time() + (86400 * 30), '/');
 }
-else if ($action == "get")
+else if ($action === "get")
 {
 	$cookie_name = $GET['name'];
 	if(($_COOKIE[$cookie_name]))
 		echo "$_COOKIE[$cookie_name]";
 }
-else if ($action == "del")
+else if ($action === "del")
 {
 	$cookie_name = $_GET['name'];
 	$cookie_value = $_GET['value'];
